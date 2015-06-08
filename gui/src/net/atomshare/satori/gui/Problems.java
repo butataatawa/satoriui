01f5c9bcbf13a4d7282d10f94636a95e99a571c7
butataatawa <jan.derbisz@gmail.com>
2015-06-08 15:08:26 +0200

    Update Problems.java

--- gui/src/net/atomshare/satori/gui/Problems.java
+++ gui/src/net/atomshare/satori/gui/Problems.java
@@ -1,25 +1,26 @@
 package net.atomshare.satori.gui;
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.control.*;
+
+        import javafx.application.Application;
+        import javafx.event.ActionEvent;
+        import javafx.event.EventHandler;
+        import javafx.geometry.Insets;
+        import javafx.geometry.Pos;
+        import javafx.scene.Scene;
+        import javafx.scene.control.Button;
+        import javafx.scene.control.Label;
+        import javafx.scene.control.PasswordField;
+        import javafx.scene.control.TextField;
+        import javafx.scene.layout.GridPane;
+        import javafx.scene.layout.HBox;
+        import javafx.scene.layout.StackPane;
+        import javafx.scene.paint.Color;
+        import javafx.scene.text.Font;
+        import javafx.scene.text.FontWeight;
+        import javafx.scene.text.Text;
+        import javafx.stage.Stage;
+        import javafx.collections.FXCollections;
+        import javafx.collections.ObservableList;
+        import javafx.scene.control.*;
 
 public class Problems extends Application{
     public static void main(String[] args) {
@@ -42,6 +43,18 @@ public class Problems extends Application{
         hbBtn.getChildren().add(btn);
         grid.add(hbBtn, 2, 3);
 
+        Button btn2 = new Button("Wyślij rozwiązanie");
+        HBox hbBtn2 = new HBox(10);
+        hbBtn2.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn2.getChildren().add(btn2);
+        grid.add(hbBtn2, 0, 3);
+
+        Button btn3 = new Button("Wyświetl wyniki");
+        HBox hbBtn3 = new HBox(10);
+        hbBtn3.setAlignment(Pos.BOTTOM_RIGHT);
+        hbBtn3.getChildren().add(btn3);
+        grid.add(hbBtn3, 1, 3);
+
         ListView<String> list = new ListView<String>();
         ObservableList<String> items =FXCollections.observableArrayList (
                 "A  Nazwa1", "B  Nazwa2", "C  Nazwa3", "D  Nazwa 4");
@@ -59,7 +72,27 @@ public class Problems extends Application{
             }
         });
 
-        Scene scene = new Scene(grid, 400, 225);
+        btn2.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                Stage stage=new Stage();
+                SubmitScreen s=new SubmitScreen();
+                s.start(stage);
+            }
+        });
+
+        btn3.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent e) {
+                Stage stage=new Stage();
+                Results r=new Results();
+                r.start(stage);
+            }
+        });
+
+        Scene scene = new Scene(grid, 500, 225);
         primaryStage.setScene(scene);
 
         Text scenetitle = new Text("Problemy");
@@ -75,4 +108,5 @@ public class Problems extends Application{
         primaryStage.show();
     }
 }
+
 
